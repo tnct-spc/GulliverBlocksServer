3d23fe7ea85f71317a74b7804fa2e5be3e424689
fazerog02 <fazerog02@gmail.com>
2019-07-09 18:27:10 +0900

    [add]ハードコーディングのブロック情報を返す機能を作った

--- api/views.py
+++ api/views.py
@@ -1,9 +1,54 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, jsonify
+from datetime import datetime
 
 
 api_app = Blueprint('api_app', __name__)
 
 
-@api_app.route('/')
-def test():
-    return "HelloWorld"
+@api_app.route('/return_test_data/')
+def return_test_data():
+    test_data = {
+        "ID": [
+            {
+                "x": 1,
+                "y": 2,
+                "z": 3,
+                "time": datetime.now().timestamp(),
+                "colorID": 1,
+                "put": True,
+            },
+            {
+                "x": 1,
+                "y": 3,
+                "z": 3,
+                "time": datetime.now().timestamp(),
+                "colorID": 1,
+                "put": True,
+            },
+            {
+                "x": 2,
+                "y": 2,
+                "z": 3,
+                "time": datetime.now().timestamp(),
+                "colorID": 1,
+                "put": True,
+            },
+            {
+                "x": 1,
+                "y": 2,
+                "z": 4,
+                "time": datetime.now().timestamp(),
+                "colorID": 1,
+                "put": True,
+            },
+            {
+                "x": 1,
+                "y": 2,
+                "z": 2,
+                "time": datetime.now().timestamp(),
+                "colorID": 1,
+                "put": True,
+            },
+        ]
+    }
+    return make_response(jsonify(test_data))
