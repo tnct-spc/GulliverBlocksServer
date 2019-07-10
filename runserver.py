b4ff280d892ad65bbe99ede3ad47057639f67d75
fazerog02 <fazerog02@gmail.com>
2019-07-11 08:48:29 +0900

    [fix]bug

--- runserver.py
+++ runserver.py
@@ -6,13 +6,8 @@ from settings import BUGSNAG_API_KEY
 if __name__ == "__main__":
     bugsnag.configure(
         api_key=BUGSNAG_API_KEY,
-        project_root="."
-    )
-
-    app.run(debug=True)
-
-    bugsnag.configure(
-        api_key=BUGSNAG_API_KEY,
         project_root=".",
         release_stage = "development",
     )
+
+    app.run(debug=True)
