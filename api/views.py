4bce33934d14d097c0191f29e144a0ce64bb9592
fazerog02 <fazerog02@gmail.com>
2019-07-11 12:14:39 +0900

    [fix]jsonの形式

--- api/views.py
+++ api/views.py
@@ -8,47 +8,45 @@ api_app = Blueprint('api_app', __name__)
 @api_app.route('/return_test_data/')
 def return_test_data():
     test_data = {
-        "ID": [
-            {
-                "x": 1,
-                "y": 2,
-                "z": 3,
-                "time": datetime.now().timestamp(),
-                "colorID": 1,
-                "put": True,
-            },
-            {
-                "x": 1,
-                "y": 3,
-                "z": 3,
-                "time": datetime.now().timestamp(),
-                "colorID": 1,
-                "put": True,
-            },
-            {
-                "x": 2,
-                "y": 2,
-                "z": 3,
-                "time": datetime.now().timestamp(),
-                "colorID": 1,
-                "put": True,
-            },
-            {
-                "x": 1,
-                "y": 2,
-                "z": 4,
-                "time": datetime.now().timestamp(),
-                "colorID": 1,
-                "put": True,
-            },
-            {
-                "x": 1,
-                "y": 2,
-                "z": 2,
-                "time": datetime.now().timestamp(),
-                "colorID": 1,
-                "put": True,
-            },
-        ]
+        "1": {
+            "x": 1,
+            "y": 2,
+            "z": 3,
+            "time": datetime.now().timestamp(),
+            "colorID": 1,
+            "put": True,
+        },
+        "2": {
+            "x": 1,
+            "y": 3,
+            "z": 3,
+            "time": datetime.now().timestamp(),
+            "colorID": 1,
+            "put": True,
+        },
+        "3": {
+            "x": 2,
+            "y": 2,
+            "z": 3,
+            "time": datetime.now().timestamp(),
+            "colorID": 1,
+            "put": True,
+        },
+        "4": {
+            "x": 1,
+            "y": 2,
+            "z": 4,
+            "time": datetime.now().timestamp(),
+            "colorID": 1,
+            "put": True,
+        },
+        "5": {
+            "x": 1,
+            "y": 2,
+            "z": 2,
+            "time": datetime.now().timestamp(),
+            "colorID": 1,
+            "put": True,
+        },
     }
     return make_response(jsonify(test_data))
