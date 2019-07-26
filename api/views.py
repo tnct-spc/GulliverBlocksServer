f8903f83f7da6ca9def7c369ea5a1060d2f5e292
lonri <tenfuux@gmail.com>
2019-07-26 23:38:01 +0900

    [add]mapのidを渡されたら、それを持ったBlockすべてをjsonで返すapi&Map一覧を返すapi

--- api/views.py
+++ api/views.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, make_response, jsonify
-from api.models import Block
+from api.models import Block, Map
 from api._db import db
 
 
 api_app = Blueprint('api_app', __name__)
 
 
-@api_app.route('/return_test_data/')
-def return_test_data():
-    blocks = db.session.query(Block)
-    test_data = {
+@api_app.route('/get_blocks/map_id:<uuid:map_id>/')
+def get_blocks(map_id):
+    blocks = db.session.query(Block).filter_by(map_id=map_id)
+    blocks_data = {
         "blocks": []
     }
 
@@ -22,6 +22,22 @@ def return_test_data():
             "y": block.y,
             "z": block.z
         }
-        test_data["blocks"].append(tmp_dic)
+        blocks_data["blocks"].append(tmp_dic)
 
-    return make_response(jsonify(test_data))
+    return make_response(jsonify(blocks_data))
+
+
+@api_app.route('/get_maps/')
+def get_maps():
+    maps = db.session.query(Map)
+    maps_data ={
+        "maps": []
+    }
+    for map in maps:
+        tmp_dic = {
+            "ID": map.id,
+            "name": map.name
+        }
+        maps_data["maps"].append(tmp_dic)
+
+    return make_response(jsonify(maps_data))
