5ca3f1727816e13ed299e7a180a2cf4b85533c08
fazerog02 <fazerog02@gmail.com>
2019-08-05 19:02:27 +0900

    [add]flask-migrateの導入

--- api/__init__.py
+++ api/__init__.py
@@ -3,10 +3,13 @@ from api._socket import sockets
 from .views import api_app
 from .websocket import ws
 from bugsnag.flask import handle_exceptions
+from flask_migrate import Migrate
+from api._db import db
 
 
 app.register_blueprint(api_app)
 sockets.register_blueprint(ws)
+Migrate(app, db)
 
 
 # bugsnag handle
