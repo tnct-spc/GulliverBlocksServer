5322c47e50d36b983b62394ff8813650c31a586b
fazerog02 <fazerog02@gmail.com>
2019-08-21 17:00:25 +0900

    [add]new table

--- api/models.py
+++ api/models.py
@@ -1,54 +1,112 @@
-from api._db import db
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from uuid import uuid4
 
 
-class Block(db.Model):
-    id = db.Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
-    x = db.Column(db.Integer, nullable=False)
-    y = db.Column(db.Integer, nullable=False)
-    z = db.Column(db.Integer, nullable=False)
-    time = db.Column(db.Float, nullable=False)
-    colorID = db.Column(db.String, nullable=False)
-    map_id = db.Column(UUID(as_uuid=True), db.ForeignKey('map.id'), nullable=False)
-
-    color_rule = db.relationship('ColorRule', backref='block', lazy=True)
+Base = declarative_base()
 
 
-class Map(db.Model):
-    id = db.Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+class Block(Base):
+    __tablename__ = 'block'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    x = Column(Integer, nullable=False)
+    y = Column(Integer, nullable=False)
+    z = Column(Integer, nullable=False)
+    time = Column(Float, nullable=False)
+    colorID = Column(String, nullable=False)
+    map_id = Column(UUID(as_uuid=True), ForeignKey('map.id'), nullable=False)
+    pattern_name = Column(String, ForeignKey('pattern.name'), nullable=True)
+    pattern_group = Column(Integer, nullable=True)
 
-    block = db.relationship('Block', backref='map', lazy=True)
-    merge_map = db.relationship('MergeMap', backref='map', lazy=True)
-    real_sense = db.relationship('RealSense', backref='map', lazy=True)
-    color_rule = db.relationship('ColorRule', backref='map', lazy=True)
+    color_rule = relationship('ColorRule', backref='block', lazy=True, cascade='all, delete-orphan')
 
-
-class RealSense(db.Model):
-    id = db.Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    current_map_id = db.Column(UUID(as_uuid=True), db.ForeignKey('map.id'), nullable=False)
-
-
-class Merge(db.Model):
-    id = db.Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    def __repr__(self):
+        return "<Block(map_id='%s')>" % self.map_id
 
 
-class MergeMap(db.Model):
-    id = db.Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
-    map_id = db.Column(UUID(as_uuid=True), db.ForeignKey('map.id'), nullable=False)
-    merge_id = db.Column(UUID(as_uuid=True), db.ForeignKey('merge.id'), nullable=False)
-    x = db.Column(db.Integer, nullable=False)
-    y = db.Column(db.Integer, nullable=False)
-    rotate = db.Column(db.Integer, nullable=False)
+class Map(Base):
+    __tablename__ = 'map'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    name = Column(String, nullable=False)
+
+    block = relationship('Block', backref='map', lazy=True, cascade='all, delete-orphan')
+    merge_map = relationship('MergeMap', backref='map', lazy=True, cascade='all, delete-orphan')
+    real_sense = relationship('RealSense', backref='map', lazy=True, cascade='all, delete-orphan')
+    color_rule = relationship('ColorRule', backref='map', lazy=True, cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return "<Map(name='%s')>" % self.name
 
 
-class ColorRule(db.Model):
-    id = db.Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
-    map_id = db.Column(UUID(as_uuid=True), db.ForeignKey('map.id'), nullable=False)
-    type = db.Column(db.String, nullable=False)
-    block_id = db.Column(UUID(as_uuid=True), db.ForeignKey('block.id'), nullable=True)
-    origin = db.Column(db.String, nullable=True)
-    to = db.Column(db.String, nullable=False)
+class RealSense(Base):
+    __tablename__ = 'real_sense'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    name = Column(String, nullable=False)
+    current_map_id = Column(UUID(as_uuid=True), ForeignKey('map.id'), nullable=False)
+
+    def __repr__(self):
+        return "<Realsense(name='%s')>" % self.name
+
+
+class Merge(Base):
+    __tablename__ = 'merge'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    name = Column(String, nullable=False)
+
+    merge_map = relationship('MergeMap', backref='merge', lazy=True, cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return "<Merge(name='%s')>" % self.name
+
+
+class MergeMap(Base):
+    __tablename__ = 'merge_map'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    map_id = Column(UUID(as_uuid=True), ForeignKey('map.id'), nullable=False)
+    merge_id = Column(UUID(as_uuid=True), ForeignKey('merge.id'), nullable=False)
+    x = Column(Integer, nullable=False)
+    y = Column(Integer, nullable=False)
+    rotate = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return "<MergeMap(merge_id='%s', map_id='%s')>" % (self.merge_id, self.map_id)
+
+
+class ColorRule(Base):
+    __tablename__ = 'color_rule'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    map_id = Column(UUID(as_uuid=True), ForeignKey('map.id'), nullable=False)
+    type = Column(String, nullable=False)
+    block_id = Column(UUID(as_uuid=True), ForeignKey('block.id'), nullable=True)
+    origin = Column(String, nullable=True)
+    to = Column(String, nullable=False)
+
+    def __repr__(self):
+        return "<ColorRule(map_id='%s', type='%s')>" % (self.map_id, self.type)
+
+
+class Pattern(Base):
+    __tablename__ = 'pattern'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    name = Column(String, nullable=False)
+
+    block = relationship('Block', backref='pattern', lazy=True, cascade='all, delete-orphan',)
+    pattern_block = relationship('PatternBlock', backref='pattern', lazy=True, cascade='all, delete-orphan',)
+
+    def __repr__(self):
+        return "<Pattern(name='%s')>" % self.name
+
+
+class PatternBlock(Base):
+    __tablename__ = 'pattern_block'
+    id = Column(UUID(as_uuid=True), default=uuid4, primary_key=True)
+    x = Column(Integer, nullable=False)
+    y = Column(Integer, nullable=False)
+    z = Column(Integer, nullable=False)
+    colorID = Column(String, nullable=False)
+    pattern_id = Column(UUID(as_uuid=True), ForeignKey('pattern.id'), nullable=False)
+
+    def __repr__(self):
+        return "<PatternBlock(pattern_id='%s')>" % self.pattern_id
--- create_seed.py
+++ create_seed.py
@@ -1,18 +1,15 @@
-from api._db import db
-from api.models import Block, Map, Merge, MergeMap, RealSense
+from api._app import db
+from api.models import Block, Map, Merge, MergeMap, RealSense, Pattern, PatternBlock
 from datetime import datetime
-
 
 if __name__ == "__main__":
     map = Map()
     map.name = "test_map"
     db.session.add(map)
     db.session.commit()
-
     realsense = RealSense(name='test_realsense', current_map_id=map.id)
     db.session.add(realsense)
     db.session.commit()
-
     blocks = [
         Block(x=1, y=2, z=3, colorID="1", time=datetime.now().timestamp(), map_id=map.id),
         Block(x=1, y=3, z=3, colorID="1", time=datetime.now().timestamp(), map_id=map.id),
@@ -20,10 +17,17 @@ if __name__ == "__main__":
         Block(x=1, y=2, z=4, colorID="1", time=datetime.now().timestamp(), map_id=map.id),
         Block(x=1, y=2, z=2, colorID="1", time=datetime.now().timestamp(), map_id=map.id)
     ]
-
     for block in blocks:
         db.session.add(block)
-
+    pattern = Pattern(name="road")
+    pattern_blocks = [
+        PatternBlock(x=0, y=0, z=0, colorID="white", pattern_id=pattern.id),
+        PatternBlock(x=1, y=0, z=0, colorID="white", pattern_id=pattern.id),
+        PatternBlock(x=2, y=0, z=0, colorID="white", pattern_id=pattern.id),
+        PatternBlock(x=3, y=0, z=0, colorID="white", pattern_id=pattern.id)
+    ]
+    for pattern_block in pattern_blocks:
+        db.session.add(pattern_block)
     try:
         db.session.commit()
     except:
