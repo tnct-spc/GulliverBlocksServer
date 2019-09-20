f63cd4073deab4f35ef6531b0967a720984d08a7
fazerog02 <fazerog02@gmail.com>
2019-09-20 18:28:52 +0900

    [fix]map_idがないと別のmapのblockが消える可能性がある

--- api/views.py
+++ api/views.py
@@ -62,7 +62,7 @@ def add_block(realsense_id):
 
     deleted_block_ids = []
     for b in delete_blocks:
-        block_object = db.session.query(Block).filter_by(x=b['x'], y=b['y'], z=b['z']).first()
+        block_object = db.session.query(Block).filter_by(x=b['x'], y=b['y'], z=b['z'], map_id=map_id).first()
         deleted_block_ids.append(block_object.id)
         db.session.delete(block_object)
     put_block_objects = [
