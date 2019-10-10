3205b7eaeb249ed37144a7d5f4ccb40545a83212
Yuto Tamura <yuto.tamur@gmail.com>
2019-10-10 17:20:43 +0900

    [fix]

--- set.py
+++ set.py
@@ -3,7 +3,6 @@ from api.models import Block, Map, Merge, MergeMap, RealSense, Pattern, PatternB
 from datetime import datetime
 
 lis = [[0,0,0]]
-map_id = input("map id =>")
 
 print("csv x,y,z, colorID\nx,y,z, colorID...")
 while True:
@@ -13,7 +12,19 @@ while True:
     lis.append(list(map(int, ins.split(","))))
 del lis[0]
 
+map_id = db.session.query(Map).order_by(Map.id.desc()).first().id
+count = 0
+for i in range(len(lis)):
+    if i % 6 == 0:
+        count += 1
+    x,y,z,cid = lis[i]
+    block = Block(x=x,y=y,z=z,colorID=str(cid), time=datetime.now().timestamp()  + 100* count, map_id=map_id)
+    db.session.add(block)
+    db.session.commit()
+
+"""
 for x,y,z,cid in lis:
     block = Block(x=x,y=y,z=z,colorID=str(cid), time=datetime.now().timestamp(), map_id=map_id)
     db.session.add(block)
     db.session.commit()
+"""
