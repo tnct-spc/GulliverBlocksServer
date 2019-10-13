c860b8ffa194aa6834350b309cd354d1a7178161
Yuto Tamura <yuto.tamur@gmail.com>
2019-10-14 01:38:16 +0900

    [for-debug]

---
+++ setPattern.py
@@ -0,0 +1,35 @@
+from api._db import db
+from api.models import Block, Map, Merge, MergeMap, RealSense, Pattern, PatternBlock
+from datetime import datetime
+
+lis = [[0,0,0]]
+
+print("pattern_name =>")
+pattern_name = input()
+pattern_id = db.session.query(Pattern).filter_by(name=pattern_name).first().id
+print("csv x,y,z, colorID\nx,y,z, colorID...")
+while True:
+    ins = input()
+    if len(ins) < 3:
+        break
+    lis.append(list(map(int, ins.split(","))))
+del lis[0]
+
+
+
+for l in lis:
+    x = l[0]
+    y = l[1]
+    z = l[2]
+    cid = l[3]
+    p = PatternBlock(x=x,y=y,z=z,colorID=str(cid), pattern_id=pattern_id)
+    db.session.add(p)
+    db.session.commit()
+
+"""
+for x,y,z,cid in lis:
+    block = Block(x=x,y=y,z=z,colorID=str(cid), time=datetime.now().timestamp(), map_id=map_id)
+    db.session.add(block)
+    db.session.commit()
+"""
+
